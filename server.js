70412c4c78921a1a183b57615c0d75924121fe0e
Avinash Singh <mailto:avinashwebappssol@gmail.com>
2023-07-19 13:11:49 +0530

    timestamp

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 //  importing the winston logging library
 const winston = require('winston');
 
+// using timestamp
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
-// Create a logger object with the desired settings
-const logger=winston.createLogger({
-    level: 'info', // logging level
-    format: winston.format.json(), //log format
-    transports:[
-        // new winston.transports.Console(), // console output
-        new winston.transports.File({filename: 'log.log',level:'error'}),  //error or error.log
-        new winston.transports.File({filename:'combined.log'}) // info or combined.log
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}:${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        label({ label: 'right meow!' }),
+        timestamp(),
+        myFormat
+    ),
+    // transports: [new transports.Console()]
+    transports: [
+        new winston.transports.Console(), // console output
+        new winston.transports.File({ filename: 'log.log', level: 'error' }),  //error or error.log
+        new winston.transports.File({ filename: 'combined.log' }) // info or combined.log
     ]
 });
 
+
 // Log an info message
-logger.info('Hello, Winston Anurag singh');
+logger.info('Hello, Winston our');
 
 // log an error message with additional metadata
-logger.error('Something went wrong here note', { error: new Error('Somthing went wrong this Avi') });
-
-// if (process.env.NODE_ENV !== 'production') {
-//     logger.add(new winston.transports.Console({
-//       format: winston.format.simple(),
-//     }));
-// }
+logger.error('Something went wrong me', { error: new Error('Somthing went wrong this Avi') });
